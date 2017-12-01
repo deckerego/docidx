f0b311ca3f44250023b1112b9e794dd3ab9e52ae
John Ellis <deckerego@gmail.com>
2017-11-30 21:29:18 -0500

    Batch processing queue with size and time limit purge modes

--- src/main/java/net/deckerego/docidx/util/BatchProcessingQueue.java
+++ src/main/java/net/deckerego/docidx/util/BatchProcessingQueue.java
@@ -2,6 +2,8 @@ package net.deckerego.docidx.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.function.Consumer;
 
@@ -9,12 +11,16 @@ public class BatchProcessingQueue<E> {
     private final ArrayBlockingQueue<E> batchQueue;
     private final int batchSize;
     private final Consumer<List<E>> callback;
+    private final Timer purgeTimer;
 
     public BatchProcessingQueue(Consumer<List<E>> callback, int batchSize, int capacity, long purgeWaitMillis) {
         this.batchSize = batchSize;
         this.callback = callback;
 
         this.batchQueue = new ArrayBlockingQueue<>(capacity);
+
+        this.purgeTimer = new Timer();
+        this.purgeTimer.scheduleAtFixedRate(new PurgeTask(), purgeWaitMillis, purgeWaitMillis);
     }
 
     public boolean offer(E element) {
@@ -33,5 +39,14 @@ public class BatchProcessingQueue<E> {
 
     public void process(List<E> batch) {
         this.callback.accept(batch);
+    }
+
+    private class PurgeTask extends TimerTask {
+        @Override
+        public void run() {
+            List<E> batch = new ArrayList<>();
+            batchQueue.drainTo(batch);
+            process(batch);
+        }
     }
 }
